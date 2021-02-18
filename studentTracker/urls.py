4f1de0fe04c7b49e67a503b58f5428a479b2f554
adriel315 <adriel315@gmail.com>
2021-02-18 11:10:24 -0500

    still at it

--- studentTracker/urls.py
+++ studentTracker/urls.py
@@ -21,9 +21,10 @@ from django.views.i18n import JavaScriptCatalog
 
 
 urlpatterns = [
-    path('/', include('tracker.urls')),
+    
     path('jsi18n', JavaScriptCatalog.as_view(), name='js-catlog'),
     path('admin/', admin.site.urls),
+    path('', include('tracker.urls')),
     
 ]
 
