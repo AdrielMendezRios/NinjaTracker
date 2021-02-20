ce855da944956a4064fb316926411bcae7f629d3
adriel315 <adriel315@gmail.com>
2021-02-20 15:31:05 -0500

    added whitenoise root

--- studentTracker/settings.py
+++ studentTracker/settings.py
@@ -158,6 +158,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT  = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+WHITENOISE_ROOT = STATIC_ROOT
 
 # Extra lookup directories for collectstatic to find static files
 # STATICFILES_DIRS = (
