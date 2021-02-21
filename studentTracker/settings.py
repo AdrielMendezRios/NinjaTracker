f2f34a699a9bf36f3c8a11be39ffda652f790ea8
adriel315 <adriel315@gmail.com>
2021-02-21 00:17:24 -0500

    added allowed hosts

--- studentTracker/settings.py
+++ studentTracker/settings.py
@@ -33,7 +33,7 @@ SESSION_COOKIE_SECURE = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '(y#+^l(@3r%-)1bbb1p=7%)#%9yu!wmioeec&ldpb=@=&0j5r(' OLD KEY
-SECRET_KEY = os.environ.get('SECRET_KEY') #creds['SECRET_KEY'] 
+SECRET_KEY = os.environ.get('SECRET_KEY') # creds['SECRET_KEY'] #
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -42,6 +42,7 @@ ALLOWED_HOSTS = [
     'localhost:8000',
     '127.0.0.1:8000',
     'ninjatracker.herokuapp.com'
+    'ninjatracker.app'
 ]
 
 
